a81362eb8cedb6173aec60180a5666fd406c266a
marcosheredia.globant@latam.com <marcosheredia.globant@latam.com>
2021-05-25 09:40:41 -0400

    add docker compose whit mysql configuration

--- src/main/java/com/example/demo/domain/Car.java
+++ src/main/java/com/example/demo/domain/Car.java
@@ -1,8 +1,21 @@
 package com.example.demo.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
+
+@Entity
+@Getter
+@Setter
 public class Car {
 
+  @Id
+  @GeneratedValue(strategy= GenerationType.AUTO)
+  long Id;
   String model;
   String Colour;
   String maximumLoad;
--- src/main/java/com/example/demo/repositori/CarRepository.java
+++ src/main/java/com/example/demo/repositori/CarRepository.java
@@ -5,5 +5,5 @@ import javax.persistence.Id;
 import org.springframework.data.repository.CrudRepository;
 
 
-public interface CarRepository extends CrudRepository<Car, Id> {
+public interface CarRepository extends CrudRepository<Car, Long> {
 }
